2a31c36ac6432787c6a8ccd52a2c2e5b51c23625
devadrianoabner <adriano.abner@live.com>
2026-06-21 01:55:28 -0300

    test: ft_printf, understood macros and structure

--- ft_printf.c
+++ ft_printf.c
@@ -1,11 +1,31 @@
 #include "ft_printf.h"
 
-int	ft_printf(const char *, ...)
+int	ft_printf(char const *format, ...)
 {
-	return (0);
-}
+	va_list	args;
+	int	num_bytes;
+	int	i;
+	int	c;
 
-int	main(void)
-{
-	ft_printf("Hello Wrold");
+	i = 0;
+	num_bytes = 0;
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				c = va_arg(args, int);
+				num_bytes = num_bytes + write(1, &c, 1);
+			}
+
+		}
+		else
+		num_bytes = num_bytes + write(1, &format[i], 1);
+		i++;
+	}
+	va_end(args);
+	return (num_bytes);
 }

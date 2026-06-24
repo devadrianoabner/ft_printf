0ad20bd129eac6166f5c13897b76108a64b072a4
Adriano Abner Da Silva Bezerra <adriano.abner@live.com>
2026-06-24 12:55:40 -0300

    add: function handle_ptr

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 15:24:19 by adrianda          #+#    #+#             */
-/*   Updated: 2026/06/23 20:16:10 by adrianda         ###   ########.fr       */
+/*   Updated: 2026/06/24 12:52:15 by adrianda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,32 @@ int	main(void)
 	returns = ft_printf("Hello Wrold 100%%\n");
 	printf("Valor retornado do ft_printf: %i\n", returns);
 	returns = printf("Hello Wrold 100%%\n");
-	printf("Valor retornado do printf: %i\n\n", returns);
+	ft_printf("Valor retornado do printf: %i\n\n", returns);
 
 
-	/// TEST C ///
+	/// TEST c ///
 	returns = ft_printf("Hello Wrold %c\n", 'H');
 	printf("Valor retornado do ft_printf: %i\n", returns);
 	returns = printf("Hello Wrold %c\n", 'H');
-	printf("Valor retornado do printf: %i\n\n", returns);
+	ft_printf("Valor retornado do printf: %i\n\n", returns);
 
-		/// TEST D ///
+		/// TEST d ///
 	returns = ft_printf("Hello Wrold %d\n", 100);
 	printf("Valor retornado do ft_printf: %i\n", returns);
 	returns = printf("Hello Wrold %d\n", 100);
-	printf("Valor retornado do printf: %i\n\n", returns);
+	ft_printf("Valor retornado do printf: %i\n\n", returns);
 	
 	/// TEST x ///
 	returns = ft_printf("Hello Wrold %x\n", -27389173);
 	printf("Valor retornado do ft_printf: %i\n", returns);
 	returns = printf("Hello Wrold %x\n", -27389173);
-	printf("Valor retornado do printf: %i\n\n", returns);
+	ft_printf("Valor retornado do printf: %i\n\n", returns);
+
+	/// TEST p ///
+	returns = ft_printf("Hello Wrold %p\n", &returns);
+	printf("Valor retornado do ft_printf: %i\n", returns);
+	returns = printf("Hello Wrold %p\n", &returns);
+	ft_printf("Valor retornado do printf: %i\n\n", returns);
 	return (0);
 }
 // cc -Wall -Wextra -Werror main.c libftprintf.a -o test_printf
--- srcs/conversions/handle_ptr.c
+++ srcs/conversions/handle_ptr.c
@@ -6,14 +6,37 @@
 /*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 15:27:36 by adrianda          #+#    #+#             */
-/*   Updated: 2026/06/23 15:38:11 by adrianda         ###   ########.fr       */
+/*   Updated: 2026/06/24 12:49:26 by adrianda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../ft_printf.h"
 
+static int	putptr_and_count(unsigned long long nbr, char *base)
+{
+	int	count;
+
+	count = 0;
+	if (nbr >= 16)
+		count += putptr_and_count(nbr / 16, base);
+	count += write(1, &base[nbr % 16], 1);
+	return (count);
+}
+
 int	handle_ptr(va_list args)
 {
-	(void)args;
-	return (0);
+	void				*ptr;
+	unsigned long long	ptr_nbr;
+	int					total_count;
+
+	ptr = va_arg(args, void *);
+	ptr_nbr = (unsigned long long) ptr;
+	if (!ptr)
+	{
+		write(1, "(nil)", 5);
+		return (5);
+	}
+	total_count = write(1, "0x", 2);
+	total_count += putptr_and_count(ptr_nbr, "0123456789abcdef");
+	return (total_count);
 }
